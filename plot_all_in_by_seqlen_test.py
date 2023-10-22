357181c9bb1f44abf4e15811d6898124b94e8bb3
ddrenteria <54510442+ddrenteria@users.noreply.github.com>
2023-10-22 18:54:59 -0300

    Add python plot scripts and images

---
+++ plot_all_in_by_seqlen_test.py
@@ -0,0 +1,147 @@
+import numpy as np
+import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Mazes to run
+mazes = [1,2,3,4]
+# Test limits to use 
+test_limits = [2000000]
+# Timeout limits to use
+timeout_limits = []
+# Sequence lengths to use
+seqLens = [10, 100, 200]
+
+OURS_ECHIDNA_PATH = "./"
+OG_ECHIDNA_PATH = "~/Desktop/tesis/og/echidna"
+RANDOM_ECHIDNA_PATH = "~/Desktop/tesis/random/echidna"
+
+MAX_TIMEOUT = 10 * 60 * 60 # 10 hours
+MAX_TESTLIMIT = 2000000000
+
+def get_test_path(testLimit, timeout_minutes, seqLen, mazeNumber, mode):
+    return get_test_path_base(testLimit, timeout_minutes, seqLen, mazeNumber) + f"/{mode}"
+
+def get_test_path_base(testLimit, timeout_minutes, seqLen, mazeNumber):
+    return f"echidna-test/seqLen_{seqLen}_timeout_{timeout_minutes}_testLim_{testLimit}_maze_{mazeNumber}"
+
+def create_plot_coverage_points(df_ours, df_og, df_random, path, seqLen):
+    df_ours = df_ours[df_ours["coverage_points"]>0]
+    df_og = df_og[df_og["coverage_points"]>0]
+    df_random = df_random[df_random["coverage_points"]>0]
+    plt.plot(np.array(df_ours["time"]), np.array(df_ours["coverage_points"]))
+    plt.plot(np.array(df_og["time"]), np.array(df_og["coverage_points"]))
+    plt.plot(np.array(df_random["time"]), np.array(df_random["coverage_points"]))
+    plt.xlabel("time (s)")
+    plt.ylabel("coverage points")
+    plt.legend(['ours', 'original', 'random'])
+    plt.title('Coverage points through time\nSequence length: ' + str(seqLen))
+    plt.savefig(path + "/average_comparison_coverage_points.jpg")
+    plt.clf()
+
+def create_plot_coverage_points_seq_number(df_ours, df_og, df_random, path, seqLen):
+    df_ours = df_ours[df_ours["coverage_points"]>0]
+    df_og = df_og[df_og["coverage_points"]>0]
+    df_random = df_random[df_random["coverage_points"]>0]
+    plt.plot(np.array(df_ours["sequence_number"]), np.array(df_ours["coverage_points"]))
+    plt.plot(np.array(df_og["sequence_number"]), np.array(df_og["coverage_points"]))
+    plt.plot(np.array(df_random["sequence_number"]), np.array(df_random["coverage_points"]))
+    plt.xlabel("# sequence")
+    plt.ylabel("coverage points")
+    plt.legend(['ours', 'original', 'random'])
+    plt.title('Coverage points through # of sequences\nSequence length: ' + str(seqLen))
+    plt.savefig(path + "/average_comparison_coverage_points_seq_number.jpg")
+    plt.clf()
+
+def create_plot_average(df_ours, df_og, df_random, path, seqLen):
+    plt.plot(np.array(df_ours["time"]), np.array(df_ours["tests_discovered"]))
+    plt.plot(np.array(df_og["time"]), np.array(df_og["tests_discovered"]))
+    plt.plot(np.array(df_random["time"]), np.array(df_random["tests_discovered"]))
+    plt.xlabel("time (s)")
+    plt.ylabel("# tests")
+    plt.legend(['ours', 'original', 'random'])
+    plt.title('Amount of tests discovered through time\nSequence length: ' + str(seqLen))
+    plt.savefig(path + "/average_comparison.jpg")
+    plt.clf()
+
+def create_data(df_ours, df_og, df_random, ar_our, ar_og, ar_random):
+    ar_og.append([float(i) for i in df_og["tests_discovered"]][-1])
+    ar_our.append([float(i) for i in df_ours["tests_discovered"]][-1])
+    ar_random.append([float(i) for i in df_random["tests_discovered"]][-1])
+    return ar_our, ar_og, ar_random
+
+def plot_data_all(ar_our, ar_og, ar_random, labels):
+    x = np.arange(len(labels))  # the label locations
+    width = 0.2  # the width of the bars
+
+    fig, ax = plt.subplots(figsize=(18,5))
+    rects1 = ax.bar(x - width, ar_our, width-0.02, label='EchidnaAfl', color="cadetblue")
+    rects2 = ax.bar(x, ar_og, width-0.02, label='Original', color="forestgreen")
+    rects3 = ax.bar(x + width, ar_random, width-0.02, label='Random', color="mediumpurple")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('# tests')
+    ax.set_xlabel('')
+    ax.set_title('# Tests discovered by contract, grouped by seqLen')
+    ax.legend(title='Version')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    fig.tight_layout()
+    # rcParams['figure.figsize'] = 15, 5
+    plt.savefig("comparison_test.jpg")
+    plt.clf()
+    #plt.show()
+
+def plot_data_afl_og(ar_our, ar_og, ar_random, labels):
+    x = np.arange(len(labels))  # the label locations
+    width = 0.3  # the width of the bars
+
+    fig, ax = plt.subplots(figsize=(18,5))
+    rects1 = ax.bar(x - (width/2), ar_our, width-0.02, label='EchidnaAfl', color="cadetblue")
+    rects3 = ax.bar(x + (width/2), ar_og, width-0.02, label='Original', color="forestgreen")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('# tests')
+    ax.set_xlabel('')
+    ax.set_title('# Tests discovered by contract, grouped by seqLen')
+    ax.legend(title='Version')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    fig.tight_layout()
+    # rcParams['figure.figsize'] = 15, 5
+    plt.savefig("comparison_afl_test.jpg")
+    plt.clf()
+    # plt.show()
+
+
+
+def get_df(path):
+    return pd.read_csv(path)
+
+# Main  ---------------------------------------------------
+ar_og = []
+ar_ours = []
+ar_random = []
+labels = []
+
+for test_limit in test_limits:
+    for seqLen in seqLens:
+        for maze in mazes:
+            df_ours = get_df(get_test_path(test_limit, MAX_TIMEOUT, seqLen, maze, "ours") + "/results_average.csv")
+            df_og = get_df(get_test_path(test_limit, MAX_TIMEOUT, seqLen, maze, "og") + "/results_average.csv")
+            df_random = get_df(get_test_path(test_limit, MAX_TIMEOUT, seqLen, maze, "random") + "/results_average.csv")
+            
+            labels.append("Maze\n" + str(maze))
+            create_data(df_ours, df_og, df_random, ar_ours, ar_og, ar_random)
+        labels.append("SeqLen " + str(seqLen))
+        ar_og.append(0)
+        ar_ours.append(0)
+        ar_random.append(0)
+        
+
+plot_data_all(ar_ours, ar_og, ar_random, labels)
+plot_data_afl_og(ar_ours, ar_og, ar_random, labels)
+            
+            
---
+++ plot_time_difference_smart_pulse.py
@@ -0,0 +1,152 @@
+import numpy as np
+import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Mazes to run
+mazes = ["Auction", "Crowdfunding", "RockPaperScissors"]
+# Test limits to use 
+test_limits = [2000000]
+# Timeout limits to use
+timeout_limits = []
+# Sequence lengths to use
+seqLens = [10, 100, 200]
+
+OURS_ECHIDNA_PATH = "./"
+OG_ECHIDNA_PATH = "~/Desktop/tesis/og/echidna"
+RANDOM_ECHIDNA_PATH = "~/Desktop/tesis/random/echidna"
+
+MAX_TIMEOUT = 10 * 60 * 60 # 10 hours
+MAX_TESTLIMIT = 2000000000
+
+def get_test_path(testLimit, timeout_minutes, seqLen, mazeNumber, mode):
+    return get_test_path_base(testLimit, timeout_minutes, seqLen, mazeNumber) + f"/{mode}"
+
+def get_test_path_base(testLimit, timeout_minutes, seqLen, mazeNumber):
+    return f"echidna-test/smart-pulse/seqLen_{seqLen}_timeout_{timeout_minutes}_testLim_{testLimit}/0.5.7/{mazeNumber}"
+
+def create_plot_coverage_points(df_ours, df_og, df_random, path, seqLen):
+    df_ours = df_ours[df_ours["coverage_points"]>0]
+    df_og = df_og[df_og["coverage_points"]>0]
+    df_random = df_random[df_random["coverage_points"]>0]
+    plt.plot(np.array(df_ours["time"]), np.array(df_ours["coverage_points"]))
+    plt.plot(np.array(df_og["time"]), np.array(df_og["coverage_points"]))
+    plt.plot(np.array(df_random["time"]), np.array(df_random["coverage_points"]))
+    plt.xlabel("time (s)")
+    plt.ylabel("coverage points")
+    plt.legend(['ours', 'original', 'random'])
+    plt.title('Coverage points through time\nSequence length: ' + str(seqLen))
+    plt.savefig(path + "/average_comparison_coverage_points.jpg")
+    plt.clf()
+
+def create_plot_coverage_points_seq_number(df_ours, df_og, path, seqLen):
+    df_ours = df_ours[df_ours["coverage_points"]>0]
+    df_og = df_og[df_og["coverage_points"]>0]
+    df_random = df_random[df_random["coverage_points"]>0]
+    plt.plot(np.array(df_ours["sequence_number"]), np.array(df_ours["coverage_points"]))
+    plt.plot(np.array(df_og["sequence_number"]), np.array(df_og["coverage_points"]))
+    plt.plot(np.array(df_random["sequence_number"]), np.array(df_random["coverage_points"]))
+    plt.xlabel("# sequence")
+    plt.ylabel("coverage points")
+    plt.legend(['ours', 'original', 'random'])
+    plt.title('Coverage points through # of sequences\nSequence length: ' + str(seqLen))
+    plt.savefig(path + "/average_comparison_coverage_points_seq_number.jpg")
+    plt.clf()
+
+def create_plot_average(df_ours, df_og, path, seqLen):
+    plt.plot(np.array(df_ours["time"]), np.array(df_ours["tests_discovered"]))
+    plt.plot(np.array(df_og["time"]), np.array(df_og["tests_discovered"]))
+    plt.plot(np.array(df_random["time"]), np.array(df_random["tests_discovered"]))
+    plt.xlabel("time (s)")
+    plt.ylabel("# tests")
+    plt.legend(['ours', 'original', 'random'])
+    plt.title('Amount of tests discovered through time\nSequence length: ' + str(seqLen))
+    plt.savefig(path + "/average_comparison.jpg")
+    plt.clf()
+
+def create_data(df_ours, df_og, ar_our, ar_og):
+    og = [float(i) for i in df_og["time"]][-1]
+    our = [float(i) for i in df_ours["time"]][-1]
+    ar_our.append((our - og)/og*100)
+    return ar_our, ar_og
+
+def plot_data_all(ar_our, ar_og, ar_random, labels):
+    x = np.arange(len(labels))  # the label locations
+    width = 0.2  # the width of the bars
+
+    fig, ax = plt.subplots(figsize=(18,5))
+    rects1 = ax.bar(x - width, ar_our, width-0.02, label='EchidnaAfl', color="cadetblue")
+    rects2 = ax.bar(x, ar_og, width-0.02, label='Original', color="forestgreen")
+    rects3 = ax.bar(x + width, ar_random, width-0.02, label='Random', color="mediumpurple")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('# tests')
+    ax.set_xlabel('')
+    ax.set_title('# Tests discovered by contract, grouped by Maze')
+    ax.legend(title='Version')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    fig.tight_layout()
+    # rcParams['figure.figsize'] = 15, 5
+    plt.savefig("comparison_maze_test.jpg")
+    plt.clf()
+    #plt.show()
+
+from matplotlib.ticker import FuncFormatter
+
+# Define a custom formatting function for the y-axis labels
+def percentage_formatter(x, pos):
+    return f"{x:.0f}%"
+
+def plot_data_afl_og(ar_our, ar_og, labels):
+    x = np.arange(len(labels))  # the label locations
+    width = 0.3  # the width of the bars
+
+    fig, ax = plt.subplots(figsize=(18,5))
+    rects1 = ax.bar(x, ar_our, 2 * width, label='EchidnaAfl', color="cadetblue")
+    # rects3 = ax.bar(x + (width/2), ar_og, width-0.02, label='Original', color="forestgreen")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Overhead percentage')
+    ax.set_xlabel('')
+    ax.set_title('Overhead percentage of EchidnaAfl over original')
+    ax.legend(title='Version')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    fig.tight_layout()
+    # rcParams['figure.figsize'] = 15, 5
+    #plt.savefig("comparison_afl_maze_test.jpg")
+    #plt.clf()
+
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(percentage_formatter))
+
+    plt.show()
+
+
+
+def get_df(path):
+    return pd.read_csv(path)
+
+# Main  ---------------------------------------------------
+ar_og = []
+ar_ours = []
+labels = []
+
+for test_limit in test_limits:
+    for seqLen in seqLens:
+        for maze in mazes:
+            df_ours = get_df(get_test_path(test_limit, MAX_TIMEOUT, seqLen, maze, "ours") + "/results_average.csv")
+            df_og = get_df(get_test_path(test_limit, MAX_TIMEOUT, seqLen, maze, "og") + "/results_average.csv")
+            
+            labels.append(str(maze))
+            create_data(df_ours, df_og, ar_ours, ar_og)
+        labels.append("SeqLen " + str(seqLen))
+        ar_og.append(0)
+        ar_ours.append(0)
+        
+# plot_data_all(ar_ours, ar_og, ar_random, labels)
+plot_data_afl_og(ar_ours, ar_og, labels)
+            
+            
